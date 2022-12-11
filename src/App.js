259dfaef663a6820ee3393bb639db1fcc3045017
Beeutifull <nemanjacvetojevic@gmail.com>
2022-12-11 20:01:40 +0100

    Added file constants and moved options

--- src/App.js
+++ src/App.js
@@ -3,23 +3,8 @@ import { v4 as uuidv4 } from "uuid";
 import { sortGamesByScore, intervalFunction } from "./utils/utils";
 
 import "./App.css";
-import { Button, InputField, ScoreBoard, Select } from "./components";
-
-const OPTIONS = [
-    { value: "giedrius", text: "Giedrius" },
-    { value: "gytis", text: "Gytis" },
-    { value: "michal", text: "Michal" },
-    { value: "viktor", text: "Viktor" },
-    { value: "nemanja", text: "Nemanja" },
-];
-
-const OPTIONS2 = [
-    { value: "giedriusaaa", text: "Giedriusaaa" },
-    { value: "gytisaaa", text: "Gytisaaa" },
-    { value: "michalaaa", text: "Michalaaa" },
-    { value: "viktoraaa", text: "Viktoraaa" },
-    { value: "nemanjaaaa", text: "Nemanjaaaa" },
-];
+import { Button, ScoreBoard, Select } from "./components";
+import { AWAY_TEAM, HOME_TEAM } from "./constants/constants";
 
 function App() {
     //useReducer here
@@ -68,14 +53,14 @@ function App() {
                     label="Home Team"
                     selected={homeTeamName}
                     onChange={setHomeTeamName}
-                    options={OPTIONS}
+                    options={HOME_TEAM}
                     placeholder="Select Home Team"
                 />
                 <Select
                     label="Away Team"
                     selected={awayTeamName}
                     onChange={setAwayTeamName}
-                    options={OPTIONS2}
+                    options={AWAY_TEAM}
                     placeholder="Select Away Team"
                 />
                 <Button onClick={handleGameStart}>Start Game</Button>
---
+++ src/constants/constants.js
@@ -0,0 +1,15 @@
+export const HOME_TEAM = [
+    { value: "serbia", text: "Serbia" },
+    { value: "england", text: "England" },
+    { value: "poland", text: "Poland" },
+    { value: "usa", text: "USA" },
+    { value: "sweden", text: "Sweden" },
+];
+
+export const AWAY_TEAM = [
+    { value: "brasil", text: "Brasil" },
+    { value: "qatar", text: "Qatar" },
+    { value: "samoa", text: "Samoa" },
+    { value: "japan", text: "Japan" },
+    { value: "southKorea", text: "South Korea" },
+];
